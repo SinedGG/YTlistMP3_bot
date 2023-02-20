96b6a153d9eef90d4d99b8e481cca3738a4309d8
Sined <denis2320032@gmail.com>
2023-02-20 14:56:05 +0200

    🍃 | New messages

--- languages/eng.js
+++ languages/eng.js
@@ -1,7 +1,12 @@
 module.exports = {
-    welcome: `Welcome 👋, I'm a bot that will help you download your music in MP3 format. \n\nIf you want to download music from any streaming platforms, just send the link in the chat.🎧\nI can also download YouTube playlists. 🎼`,
-    notFound:
-      "Unfortunately, nothing was found 🥲 \nMaybe it's not available in my region.",
-    notFoundOther:
-      "Unfortunately, nothing was found 🥲 \nFor a more accurate search, use the Youtube link.🎶",
- };
+  welcome: `Welcome 👋, I'm a bot that will help you download your music in MP3 format. \n\nIf you want to download music from any streaming platforms, just send the link in the chat.🎧\nI can also download YouTube playlists. 🎼`,
+  oneLoadding: "Loading...🎶",
+  playlistRandom: "This is a randomly generated playlist so tracks may vary🎶",
+  playlistError: "Sorry, I can't download this playlist 🥲",
+  playlistStart:
+    "Playlist loading has started 🎶\nTo stop the download, use the /cancel command",
+  notFound:
+    "Unfortunately, nothing was found 🥲 \nMaybe the track is not available in my region.",
+  notFoundOther:
+    "Unfortunately, nothing was found 🥲 \nFor a more accurate search, use the Youtube link.🎶",
+};
