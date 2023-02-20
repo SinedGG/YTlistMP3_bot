46708004b3c45f767c18e46ffa76ef4f467430e6
Sined <denis2320032@gmail.com>
2023-02-20 09:31:50 +0200

    🍃 |  Create pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    apps : [{
+      name   : "[TG] YTlistMP3",
+      script : "./index.js",
+      exec_mode : "cluster",
+      log_date_format : "YYYY-MM-DD HH:mm "
+    }]
+  }
