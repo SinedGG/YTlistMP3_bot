b11d10b3a6024ba0b9a4e0da22f6c4d5cdc18fb0
Sined <denis2320032@gmail.com>
2023-02-20 15:02:54 +0200

    🍃 | Add error catching

--- commands/list.js
+++ commands/list.js
@@ -4,15 +4,19 @@ const sendMessage = require("../module/sendMessage");
 module.exports = {
   name: "list",
   execute(ctx) {
-    const url = ctx.message.text.split(" ")[1];
-    const list_id = new URL(url).searchParams.get("list");
-    var oneTime = false;
-    if (url.includes("list=RD")) {
-      sendMessage(ctx, "playlistRandom", true);
-      oneTime = true;
-    }
+    try {
+      const url = ctx.message.text.split(" ")[1];
+      const list_id = new URL(url).searchParams.get("list");
+      var oneTime = false;
+      if (url.includes("list=RD")) {
+        sendMessage(ctx, "playlistRandom", true);
+        oneTime = true;
+      }
 
-    list.load(ctx, list_id, oneTime);
+      list.load(ctx, list_id, oneTime);
+    } catch (error) {
+      sendMessage(ctx, "playlistError", true);
+    }
 
     setTimeout(() => {
       bot.telegram.deleteMessage(ctx.chat.id, ctx.message.message_id);
