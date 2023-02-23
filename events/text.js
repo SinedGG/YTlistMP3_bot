f18ed28d78aead1bcf2cba2aa07dbbb4f5daba40
Sined <denis2320032@gmail.com>
2023-02-23 20:58:59 +0200

    🍃 | Saving user id on message event

--- events/text.js
+++ events/text.js
@@ -1,11 +1,17 @@
 const getSong = require("../module/getSong.js");
 
+const db = require("../db");
 const getLinkFromOther = require("../module/getLinkFromOther.js");
 const list = require("../module/loadList.js");
 
 module.exports = {
   name: "text",
   execute(ctx) {
+    db(`INSERT INTO users (tg_id, tg_username) VALUES (?, ?)`, [
+      ctx.chat.id,
+      ctx.chat.username,
+    ]).catch((err) => console.log(err.message));
+
     const content = ctx.message.text;
     if (content.includes("youtube.com") || content.includes("youtu.be")) {
       if (content.includes("playlist?")) {
