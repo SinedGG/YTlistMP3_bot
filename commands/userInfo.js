769591770e850ba5c7efb7ea6690a5488f333780
Sined <denis2320032@gmail.com>
2023-02-23 11:28:02 +0200

    🍃 | User info by id

---
+++ commands/userInfo.js
@@ -0,0 +1,24 @@
+const ifAdmin = require("../module/ifAdmin");
+
+module.exports = {
+  name: "userinfo",
+  async execute(ctx) {
+    try {
+      if (admin(ctx.chat.id)) return;
+
+      const id = ctx.message.text.split(" ")[1];
+      const info = await bot.telegram.getChat(id);
+      ctx.reply(
+        `User Info:
+        ID: ${info.id}
+        Username: ${info.username}
+        First Name: ${info.first_name}
+        Last Name: ${info.last_name}
+        Bio: ${info.bio}
+        `
+      );
+    } catch (error) {
+      ctx.reply("Error: " + error.message);
+    }
+  },
+};
